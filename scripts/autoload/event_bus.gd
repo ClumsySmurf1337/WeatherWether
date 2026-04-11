1e567066cf0a1444289c5f39d26340cb5bef4a45
ClumsySmurf1337 <151935406+ClumsySmurf1337@users.noreply.github.com>
2026-04-11 14:09:08 -0400

    WEA-529: lane work (#28)

--- scripts/autoload/event_bus.gd
+++ scripts/autoload/event_bus.gd
@@ -2,40 +2,40 @@
 ## Registered as autoload in project.godot.
 ##
 ## Signals:
-## - level_started(level_id: String)
+## - level_started(level: LevelData)
 ## - level_completed(level_id: String, stars: int, moves_used: int)
 ## - level_failed(level_id: String, cause: int)
 ## - card_queued(card_type: int, pos: Vector2i)
 ## - card_unqueued(index: int)
-## - queue_cleared
-## - sequence_started
+## - queue_cleared()
+## - sequence_started()
 ## - sequence_card_resolved(card_type: int, pos: Vector2i)
-## - sequence_finished
-## - walk_started
+## - sequence_finished()
+## - walk_started()
 ## - walk_step(pos: Vector2i)
 ## - character_died(cause: int)
-## - character_won
-## - no_path_forward
+## - character_won()
+## - no_path_forward()
 ## - settings_changed(key: String, value: Variant)
 extends Node
 
-signal level_started(level_id: String)
+signal level_started(level: LevelData)
 signal level_completed(level_id: String, stars: int, moves_used: int)
 signal level_failed(level_id: String, cause: int)
 
 signal card_queued(card_type: int, pos: Vector2i)
 signal card_unqueued(index: int)
-signal queue_cleared
+signal queue_cleared()
 
-signal sequence_started
+signal sequence_started()
 signal sequence_card_resolved(card_type: int, pos: Vector2i)
-signal sequence_finished
+signal sequence_finished()
 
-signal walk_started
+signal walk_started()
 signal walk_step(pos: Vector2i)
 signal character_died(cause: int)
-signal character_won
+signal character_won()
 
-signal no_path_forward
+signal no_path_forward()
 
 signal settings_changed(key: String, value: Variant)
